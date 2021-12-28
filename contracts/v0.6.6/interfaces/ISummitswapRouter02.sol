7864a406c50eeb6af306518d7155266f72030c18
erwin-huang <erwinhuank@gmail.com>
2021-12-28 10:18:56 +0700

    add SummitSwapRouter Project to v0.6.6 folder

--- contracts/v0.6.6/interfaces/ISummitswapRouter02.sol
+++ contracts/v0.6.6/interfaces/ISummitswapRouter02.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.6.2;
 import './ISummitswapRouter01.sol';
 
 interface ISummitswapRouter02 is ISummitswapRouter01 {
-    function summitReferral() external pure returns (address);
-
     function removeLiquidityETHSupportingFeeOnTransferTokens(
         address token,
         uint liquidity,
@@ -23,6 +21,7 @@ interface ISummitswapRouter02 is ISummitswapRouter01 {
         bool approveMax, uint8 v, bytes32 r, bytes32 s
     ) external returns (uint amountETH);
 
+    // Supporting Fee cause We are sending fee
     function swapExactTokensForTokensSupportingFeeOnTransferTokens(
         uint amountIn,
         uint amountOutMin,

d314f4091177f7ea3ce597729104f7ecf763d188
erwin-huang <erwinhuank@gmail.com>
2022-01-03 15:43:00 +0700

    add test for SummitReferral

---
+++ test/SummitReferral.test.ts
@@ -0,0 +1,294 @@
+import {expect, use, assert} from 'chai';
+import {Contract, utils} from 'ethers';
+import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
+import SummitReferral from '../build/SummitReferral.json';
+import WETH from '../build/WBNB.json';
+import Token from '../build/DummyToken.json';
+import SummitswapFactory from '../build/SummitswapFactory.json';
+import SummitswapRouter02 from '../build/SummitswapRouter02.json';
+
+use(solidity);
+
+describe("Summit Referral", () => {
+  const [owner, leadInfluencer, subInfluencer, otherWallet, otherWallet2] = new MockProvider().getWallets();
+  let weth: Contract;
+  let summitswapFactory: Contract;
+  let summitswapRouter02: Contract;
+  let summitReferral: Contract;
+  let tokenA: Contract;
+  let tokenB: Contract;
+  let tokenR: Contract;
+
+  before(async () => {
+    weth = await deployContract(owner, WETH, []);
+    tokenA = await deployContract(owner, Token, []);
+    tokenB = await deployContract(owner, Token, []);
+    tokenR = await deployContract(owner, Token, []);
+    summitswapFactory = await deployContract(owner, SummitswapFactory, [owner.address]);
+    summitswapRouter02 = await deployContract(
+      owner, SummitswapRouter02, [summitswapFactory.address, weth.address], {gasLimit: 4612388}
+    );
+    summitReferral = await deployContract(owner, SummitReferral, []);
+
+    await summitswapFactory.setFeeTo(owner.address);
+    await summitswapRouter02.setSummitReferral(summitReferral.address);
+  });
+
+  it("Owner should be owner.address", async () => {
+    assert.equal(await summitReferral.owner(), owner.address);
+  });
+
+  it("SummitSwapRouter should have summitReferral address", async () => {
+    assert.equal(await summitswapRouter02.summitReferral(), summitReferral.address);
+  });
+
+  it("feeDenominator should be 10000", async () => {
+    let feeDenominator = await summitReferral.feeDenominator();
+    assert.equal(feeDenominator, 10000);
+  });
+
+  it("wallet should able to recordReferral", async () => {
+    await summitReferral.recordReferral(otherWallet.address, leadInfluencer.address);
+    let wallet1Referrer = await summitReferral.getReferrer(otherWallet.address);
+    assert.equal(wallet1Referrer, leadInfluencer.address);
+  });
+
+  it("setFirstBuyFee to 1000", async () => {
+    await summitReferral.setFirstBuyFee(tokenA.address, 500);
+    let firstBuyFeeTokenA = await summitReferral.getFirstBuyFee(tokenA.address);
+    assert.equal(firstBuyFeeTokenA, 500);
+
+    await summitReferral.setFirstBuyFee(weth.address, 500);
+    let firstBuyFeeWETH = await summitReferral.getFirstBuyFee(weth.address);
+    assert.equal(firstBuyFeeWETH, 500);
+  });
+
+  it ("setFirstBuyFee value should be less than feeDenominator (1000)", async () => {
+    await expect(
+      summitReferral.setFirstBuyFee(tokenA.address, 1001)
+    ).to.be.revertedWith("Wrong Fee");
+  })
+
+  it("setFirstBuyFee with otherWallet (Not The Owner) should be reverted", async () => {
+    await expect(
+      summitReferral.connect(otherWallet).setFirstBuyFee(tokenA.address, 1000)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("setDevAddress to wallet", async () => {
+    await summitReferral.setDevAddress(owner.address);
+
+    let devAddress = await summitReferral.getDevAddr();
+    assert.equal(devAddress, owner.address);
+  });
+
+  it("setDevAddress with otherWallet (Not The Owner) should be reverted", async () => {
+    await expect(
+      summitReferral.connect(otherWallet).setDevAddress(otherWallet.address)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("setRouter to summitswapRouter02.address", async () => {
+    await summitReferral.setRouter(summitswapRouter02.address);
+
+    let router = await summitReferral.getRouter();
+    assert.equal(router, summitswapRouter02.address);
+  });
+
+  it("setRouter with otherWallet (Not The Owner) should be reverted", async () => {
+    await expect(
+      summitReferral.connect(otherWallet).setRouter(summitswapRouter02.address)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("refFee + devFee in setFeeInfo which is greater than feeDenominator should be reverted", async () => {
+    await summitswapFactory.createPair(tokenA.address, tokenB.address);
+    let pairAddress = await summitswapFactory.getPair(tokenA.address, tokenB.address);
+
+    await expect(
+      summitReferral.setFeeInfo(pairAddress, tokenR.address, 5000, 6000)
+    ).to.be.revertedWith("Wrong Fee");
+  });
+
+  it("setFeeInfo to TokenB, TokenR, refFee=500, devFee=500", async () => {
+    await summitReferral.setFeeInfo(tokenB.address, tokenR.address, 500, 500);
+    let pairInfo = await summitReferral.pairInfo(tokenB.address);
+    assert.equal(pairInfo.tokenR, tokenR.address);
+    assert.equal(pairInfo.refFee, 500);
+    assert.equal(pairInfo.devFee, 500);
+  });
+
+  it("setFeeInfo with otherWallet (Not The Owner) should be reverted", async () => {
+    await expect(
+      summitReferral.connect(otherWallet).setFeeInfo(tokenB.address, tokenR.address, 30, 4)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("add LeadInfluencer with fee greater than feeDenominator should be reverted", async () => {
+    await expect(
+      summitReferral.addLeadInfluencer(owner.address, 10001)
+    ).to.be.revertedWith("Wrong Fee");
+  });
+
+  it("add owner as Lead Influencer", async () => {
+    await summitReferral.addLeadInfluencer(owner.address, 500);
+    let leadInfluencer = await summitReferral.leadInfluencers(owner.address);
+    assert.equal(leadInfluencer, true);
+
+    let leadInfFee = await summitReferral.leadInfFee(owner.address);
+    assert.equal(leadInfFee, 500);
+  });
+
+  it("addLeadInfluencer with otherWallet (Not The Owner) should be reverted", async () => {
+    await expect(
+      summitReferral.connect(otherWallet).addLeadInfluencer(otherWallet.address, 100)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("remove owner from Lead Influencer", async () => {
+    await summitReferral.removeLeadInfluencer(owner.address);
+    let leadInfluencer = await summitReferral.leadInfluencers(owner.address);
+    assert.equal(leadInfluencer, false);
+  });
+
+  it("removeLeadInfluencer with otherWallet (Not The Owner) should be reverted", async () => {
+    await expect(
+      summitReferral.connect(otherWallet).removeLeadInfluencer(owner.address)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("addSubInfluencer should only be called by Lead Influencer", async () => {
+    await expect(
+      summitReferral.addSubInfluencer(subInfluencer.address, 5, 5)
+    ).to.be.revertedWith("No permission to add influencer");
+  });
+
+  it("LeadInfluencer Should not be able to add LeadInfluencer as SubInfluencer", async () => {
+    await summitReferral.addLeadInfluencer(owner.address, 5);
+    await summitReferral.addLeadInfluencer(leadInfluencer.address, 5);
+
+    await expect(
+      summitReferral.addSubInfluencer(leadInfluencer.address, 5, 5)
+    ).to.be.revertedWith("Not able to add lead influencer as a sub influencer");
+  });
+
+  it("addSubInfluencer _leadFee + _infFee should be 1000", async () => {
+    await expect(
+      summitReferral.addSubInfluencer(subInfluencer.address, 5, 5)
+    ).to.be.revertedWith("Wrong Fee");
+  });
+
+  it("LeadInfluencer should able to add SubInfluencer", async () => {
+    await summitReferral.connect(leadInfluencer).addSubInfluencer(subInfluencer.address, 500, 500);
+    let influencer =  await summitReferral.influencers(subInfluencer.address);
+    assert.equal(influencer.leadAddress, leadInfluencer.address);
+    assert.equal(influencer.refFee, 500);
+    assert.equal(influencer.leadFee, 500);
+  });
+
+  it("Other LeadInfluencer should not be able to add SubInfluencer that already added by another lead", async () => {
+    await expect(
+      summitReferral.addSubInfluencer(subInfluencer.address, 500, 500)
+    ).to.be.revertedWith("This address is already added by another lead");
+  });
+
+  it("SubInfluencer should not be added as Lead Influencer", async () => {
+    await expect(
+      summitReferral.addLeadInfluencer(subInfluencer.address, 500)
+    ).to.be.revertedWith("Not able to add sub influencer as a lead influencer");
+  });
+
+  it("swap which is not called by router should be reverted", async () => {
+    await expect(
+      summitReferral.swap(owner.address, tokenA.address, tokenB.address, 100, 0)
+    ).to.be.revertedWith("caller is not the router");
+  });
+
+  it("User will be able to swap eth to exact token, and get the first reward", async () => {
+    await tokenA.approve(summitswapRouter02.address, utils.parseEther('5').toString());
+    await tokenR.approve(summitswapRouter02.address, utils.parseEther('5').toString());
+
+    await summitswapRouter02.addLiquidityETH(
+      tokenA.address, // address token,
+      utils.parseEther('5'), // uint amountTokenDesired,
+      utils.parseEther('5'), // uint amountTokenMin,
+      utils.parseEther('0.1'), // uint amountETHMin,
+      owner.address, // address to
+      Math.floor(Date.now()/1000) + 24 * 60 * 60, // uint deadline
+      {value: utils.parseEther('0.1')}
+    );
+
+    await summitswapRouter02.addLiquidityETH(
+      tokenR.address, // address token,
+      utils.parseEther('5'), // uint amountTokenDesired,
+      utils.parseEther('5'), // uint amountTokenMin,
+      utils.parseEther('0.1'), // uint amountETHMin,
+      owner.address, // address to
+      Math.floor(Date.now()/1000) + 24 * 60 * 60, // uint deadline
+      {value: utils.parseEther('0.1')}
+    );
+
+    let pairAddress = await summitswapFactory.getPair(weth.address, tokenA.address);
+    await summitReferral.setFeeInfo(pairAddress, tokenR.address, 500, 500);
+
+    let amount = await summitswapRouter02.getAmountsOut(utils.parseEther('0.1'), [weth.address, tokenA.address]);
+    let amountOut = amount[0];
+    let amountIn = amount[1];
+
+    // 2046957198124988 - 100000000000000000
+    await summitswapRouter02.connect(otherWallet).swapETHForExactTokens(
+      amountOut, // uint amountOut
+      [weth.address, tokenA.address], // address[] calldata path
+      owner.address, // address to
+      Math.floor(Date.now()/1000) + 24 * 60 * 60, // uint deadline
+      {value: amountIn}
+    );
+
+    let swapList = await summitReferral.getSwapList(leadInfluencer.address);
+    let swapInfo = swapList[0];
+
+    assert.equal(swapInfo.tokenA, weth.address);
+    assert.equal(swapInfo.tokenB, tokenA.address);
+    assert.equal(swapInfo.tokenR, tokenR.address);
+    assert.equal(swapInfo.amountA, 2046957198124988);
+    assert.equal(swapInfo.amountB, 100000000000000000);
+    assert.equal(swapInfo.amountR, 5000000000000001);
+    assert.equal(swapInfo.amountD, 5000000000000001);
+  });
+
+  it("reward for the user (first claim) should be 50000000000000018", async () => {
+    let rewardBalance = await summitReferral.rewardBalance(otherWallet.address, tokenR.address);
+    assert.equal(rewardBalance.toString(), 50000000000000018);
+  });
+  it("tokenR rewardBalance for Referrer should be 5000000000000001 wei", async () => {
+    let rewardBalance = await summitReferral.rewardBalance(leadInfluencer.address, tokenR.address);
+    assert.equal(rewardBalance.toString(), 5000000000000001);
+  });
+
+  it("tokenR rewardBalance for Developer should be 5000000000000001 wei", async () => {
+    let rewardBalance = await summitReferral.rewardBalance(owner.address, tokenR.address);
+    assert.equal(rewardBalance.toString(), 5000000000000001);
+  });
+
+  it("leadInfluencer should be able to claim reward", async () => {
+    await tokenR.transfer(summitReferral.address, utils.parseEther('10').toString());
+
+    let rewardBalance = await summitReferral.rewardBalance(leadInfluencer.address, tokenR.address);
+    await summitReferral.connect(leadInfluencer).claimReward(tokenR.address);
+
+    let walletTokenRAmount = await tokenR.balanceOf(leadInfluencer.address);
+    assert.equal(rewardBalance.toString(), walletTokenRAmount.toString());
+  });
+
+  it("leadInfluencer should not be able to claim reward when balance is 0", async () => {
+    await expect(
+      summitReferral.connect(leadInfluencer).claimReward(tokenR.address)
+    ).to.be.revertedWith("Insufficient balance");
+  });
+
+  it("tokenR rewardBalance for OtherWallet should be 0", async () => {
+    let rewardBalance = await summitReferral.rewardBalance(otherWallet.address, tokenR.address);
+    assert.equal(rewardBalance, 0);
+  });
+
+})

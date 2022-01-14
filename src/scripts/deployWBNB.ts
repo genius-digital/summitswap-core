e2a3345c5f2a1d0b498105681cef88e3780fbbb3
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-01-14 18:55:07 +0400

    feat(deploywbnb-script); verify after deployment

--- src/scripts/deployWBNB.ts
+++ src/scripts/deployWBNB.ts
@@ -1,11 +1,14 @@
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
 
 async function main() {
   const WBNB = await ethers.getContractFactory("WBNB");
   const wbnb = await WBNB.deploy();
-  await wbnb.deployed();
 
   console.log("Contract deployed to:", wbnb.address);
+
+  await hre.run("verify:verify", {
+    address: wbnb.address,
+  });
 }
 
 main();

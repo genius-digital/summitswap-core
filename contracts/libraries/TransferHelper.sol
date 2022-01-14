792645ce01f3ffbe211c5095135775bf4158f196
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-01-14 06:01:42 +0400

    refactor: remove unnecessary comments

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
 pragma solidity >=0.6.0;
 
 // helper methods for interacting with BEP20 tokens and sending ETH that do not consistently return true/false
--- contracts/utils/DummyToken.sol
+++ contracts/utils/DummyToken.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity =0.6.6;
 
 import "../shared/ERC20.sol";

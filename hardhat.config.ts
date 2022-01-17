515efef2203c65d9e1cc1349cfdf0687714f17f9
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-01-17 14:19:21 +0400

    chores(hardhat-confgi): use same optimizer for all sol versions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -32,9 +32,21 @@ const config: HardhatUserConfig = {
     compilers: [
       {
         version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
       {
         version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
       {
         version: "0.6.6",

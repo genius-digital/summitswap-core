d5ce6da4d8fde684e57c616739a81fe849c85fdb
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-01-17 21:08:30 +0400

    feat(referral): refactor & add suport for referring per token

--- src/contracts/SummitReferral.sol
+++ src/contracts/SummitReferral.sol
@@ -8,52 +8,58 @@ import "./interfaces/ISummitswapRouter02.sol";
 
 import "./shared/Ownable.sol";
 
+struct FeeInfo {
+  address tokenR;
+  uint256 refFee;
+  uint256 devFee;
+}
+
+struct InfInfo {
+  address leadAddress;
+  uint256 refFee;
+  uint256 leadFee;
+}
+
+struct SwapInfo {
+  uint256 timestamp;
+  address tokenA;
+  address tokenB;
+  address tokenR; // reward token
+  uint256 amountA; // tokenA amount
+  uint256 amountB; // tokenB amount
+  uint256 amountR; // Ref amount
+  uint256 amountD; // Dev amount
+}
+
 contract SummitReferral is Ownable {
   using SafeMath for uint256;
 
-  struct FeeInfo {
-    address tokenR;
-    uint256 refFee;
-    uint256 devFee;
-  }
-  mapping(address => FeeInfo) public pairInfo; // pair address => fee info
   uint256 public feeDenominator = 1000000000; // fee denominator
 
-  struct InfInfo {
-    address leadAddress;
-    uint256 refFee;
-    uint256 leadFee;
-  }
+  address public router;
+  address public devAddr;
+
+  mapping(address => FeeInfo) public pairInfo; // pair address => fee info
+
   mapping(address => bool) public leadInfluencers;
   mapping(address => uint256) public leadInfFee;
   mapping(address => InfInfo) public influencers;
 
-  struct SwapInfo {
-    uint256 timestamp;
-    address tokenA;
-    address tokenB;
-    address tokenR; // reward token
-    uint256 amountA; // tokenA amount
-    uint256 amountB; // tokenB amount
-    uint256 amountR; // Ref amount
-    uint256 amountD; // Dev amount
-  }
   mapping(address => SwapInfo[]) private swapList; // refer address => swap info
 
   mapping(address => bool) private rewardEnabled;
+
+  // TODO: Ask do we need this?
   address[] private rewardTokens;
 
-  mapping(address => mapping(address => uint256)) _balances; // address => token => amount
-
-  address public router; // dex router
-
-  address devAddr; // dev address
-
+  mapping(address => mapping(address => uint256)) public _balances; // address => token => amount
   mapping(address => mapping(address => bool)) public firstBuy;
-  mapping(address => uint256) firstBuyFee;
-  mapping(address => address) public referrers; // user address => referrer address
-  mapping(address => uint256) public referralsCount; // referrer address => referrals count
+  mapping(address => uint256) public firstBuyFee;
+  mapping(address => mapping(address => address)) public referrers; // referree => token => referrer
   mapping(address => uint256) public totalSharedReward; // tokenR address => total reward amount
+
+  // TODO: Ask do we need this?
+  mapping(address => uint256) public referralsCount; // referrer address => referrals count
 
   event ReferralRecorded(address indexed user, address indexed referrer);
   event ReferralReward(
@@ -70,104 +76,57 @@ contract SummitReferral is Ownable {
     _;
   }
 
-  constructor() public {}
-
-  function recordReferral(address _user, address _referrer) external {
-    if (_user != address(0) && _referrer != address(0) && _user != _referrer && referrers[_user] == address(0)) {
-      referrers[_user] = _referrer;
+  // TODO: I should not be able to call this function with another user's address
+  function recordReferral(
+    address _user,
+    address _token,
+    address _referrer
+  ) external {
+    // TODO: Ask do we need to check (_user != address(0) && _referrer != address(0))?
+    if (
+      _user != address(0) && _referrer != address(0) && _user != _referrer && referrers[_user][_token] == address(0)
+    ) {
+      referrers[_user][_token] = _referrer;
       referralsCount[_referrer] += 1;
       emit ReferralRecorded(_user, _referrer);
     }
   }
 
-  // Get the referrer address that referred the user
-  function getReferrer(address _user) external view returns (address) {
-    return referrers[_user];
-  }
-
-  function setFirstBuyFee(address _token, uint256 _fee) external onlyOwner {
+  function addLeadInfluencer(address _user, uint256 _fee) external onlyOwner {
     require(_fee <= feeDenominator, "Wrong Fee");
-    firstBuyFee[_token] = _fee;
+    require(influencers[_user].leadAddress == address(0), "Not able to add sub influencer as a lead influencer");
+    leadInfluencers[_user] = true;
+    leadInfFee[_user] = _fee;
   }
 
-  function setDevAddress(address _devAddr) external onlyOwner {
-    devAddr = _devAddr;
+  function removeLeadInfluencer(address _influencer) external onlyOwner {
+    leadInfluencers[_influencer] = false;
   }
 
-  function setRouter(address _router) external onlyOwner {
-    router = _router;
-  }
-
-  function setFeeInfo(
-    address _pair,
-    address _rewardToken,
-    uint256 _refFee,
-    uint256 _devFee
-  ) external onlyOwner {
-    require(_refFee + _devFee <= feeDenominator, "Wrong Fee");
-    pairInfo[_pair].tokenR = _rewardToken;
-    pairInfo[_pair].refFee = _refFee;
-    pairInfo[_pair].devFee = _devFee;
-  }
-
-  function getFirstBuyFee(address _token) external view onlyOwner returns (uint256) {
-    return firstBuyFee[_token];
-  }
-
-  function getDevAddr() external view onlyOwner returns (address) {
-    return devAddr;
-  }
-
-  function getRouter() external view onlyOwner returns (address) {
-    return router;
-  }
-
-  function addLeadInfluencer(address _inf, uint256 _fee) external onlyOwner {
-    require(_fee <= feeDenominator, "Wrong Fee");
-    require(influencers[_inf].leadAddress == address(0), "Not able to add sub influencer as a lead influencer");
-    leadInfluencers[_inf] = true;
-    leadInfFee[_inf] = _fee;
-  }
-
-  function removeLeadInfluencer(address _inf) external onlyOwner {
-    leadInfluencers[_inf] = false;
-  }
-
+  // TODO: Ask if leadinfluencer could add anyone as their subinfluencer without their consent
   function addSubInfluencer(
-    address _inf,
+    address _user,
     uint256 _leadFee,
     uint256 _infFee
   ) external {
     require(leadInfluencers[msg.sender] == true, "No permission to add influencer");
-    require(leadInfluencers[_inf] == false, "Not able to add lead influencer as a sub influencer");
-    require(influencers[_inf].leadAddress == address(0), "This address is already added by another lead");
+    require(leadInfluencers[_user] == false, "Not able to add lead influencer as a sub influencer");
+    require(
+      influencers[_user].leadAddress == address(0) || influencers[_user].leadAddress == msg.sender,
+      "This address is already added by another lead"
+    );
     require(_leadFee + _infFee == feeDenominator, "Wrong Fee");
-    influencers[_inf].leadAddress = msg.sender;
-    influencers[_inf].refFee = _infFee;
-    influencers[_inf].leadFee = _leadFee;
+    influencers[_user].leadAddress = msg.sender;
+    influencers[_user].refFee = _infFee;
+    influencers[_user].leadFee = _leadFee;
   }
 
-  function changeSubInfluencer(
-    address _inf,
-    uint256 _leadFee,
-    uint256 _infFee
-  ) external {
-    require(influencers[_inf].leadAddress == msg.sender, "No permission to change this influencer");
-    require(_leadFee + _infFee == feeDenominator, "Wrong Fee");
-    influencers[_inf].refFee = _infFee;
-    influencers[_inf].leadFee = _leadFee;
-  }
-
-  function rewardBalance(address user, address token) external view returns (uint256) {
-    return _balances[user][token];
-  }
-
-  function claimReward(address token) external {
-    uint256 balance = _balances[msg.sender][token];
+  function claimReward(address _rewardToken) external {
+    uint256 balance = _balances[msg.sender][_rewardToken];
     require(balance > 0, "Insufficient balance");
-    IERC20(token).transfer(msg.sender, balance);
-    _balances[msg.sender][token] = 0;
-    totalSharedReward[token] = totalSharedReward[token] - balance;
+    _balances[msg.sender][_rewardToken] = 0;
+    totalSharedReward[_rewardToken] -= balance;
+    IERC20(_rewardToken).transfer(msg.sender, balance);
   }
 
   function getReward(
@@ -204,7 +163,7 @@ contract SummitReferral is Ownable {
     uint256 _amountA,
     uint256 _amountB
   ) external onlyRouter {
-    address referrer = referrers[user];
+    address referrer = referrers[user][_tokenB];
     if (referrer == address(0)) {
       return;
     }
@@ -230,9 +189,7 @@ contract SummitReferral is Ownable {
     if (influencers[referrer].leadAddress != address(0)) {
       uint256 amountI = amountReward.mul(leadInfFee[influencers[referrer].leadAddress]).div(feeDenominator);
       amountL = amountI.mul(influencers[referrer].leadFee).div(feeDenominator);
-      _balances[influencers[referrer].leadAddress][rewardToken] =
-        _balances[influencers[referrer].leadAddress][rewardToken] +
-        amountL;
+      _balances[influencers[referrer].leadAddress][rewardToken] += amountL;
       swapList[influencers[referrer].leadAddress].push(
         SwapInfo({
           timestamp: block.timestamp,
@@ -242,7 +199,7 @@ contract SummitReferral is Ownable {
           amountA: _amountA,
           amountB: _amountB,
           amountR: amountL,
-          amountD: 0
+          amountD: 0 // TODO: Why do we throw this event with amountD: 0, on line 227 we calculate amountD
         })
       );
       amountR = amountI.mul(influencers[referrer].refFee).div(feeDenominator);
@@ -250,14 +207,17 @@ contract SummitReferral is Ownable {
       amountR = amountReward.mul(pairInfo[pair].refFee).div(feeDenominator);
     }
 
-    uint256 amountD = amountReward.mul(pairInfo[pair].devFee).div(feeDenominator);
     if (firstBuy[user][_tokenA] == false) {
+      firstBuy[user][_tokenA] = true;
       amountU = amountReward.mul(firstBuyFee[_tokenA]);
       amountU = amountU.div(feeDenominator);
-      rewardUser(user, _tokenA, rewardToken, amountU);
+      _balances[user][rewardToken] += amountU;
     }
-    _balances[referrer][rewardToken] = _balances[referrer][rewardToken] + amountR;
-    _balances[devAddr][rewardToken] = _balances[devAddr][rewardToken] + amountD;
+
+    uint256 amountD = amountReward.mul(pairInfo[pair].devFee).div(feeDenominator);
+
+    _balances[referrer][rewardToken] += amountR;
+    _balances[devAddr][rewardToken] += amountD;
 
     swapList[referrer].push(
       SwapInfo({
@@ -276,21 +236,36 @@ contract SummitReferral is Ownable {
     emit ReferralReward(user, rewardToken, amountL, amountR, amountU, amountD);
   }
 
-  function rewardUser(
-    address _user,
-    address _tokenA,
-    address _rewardToken,
-    uint256 _amountReward
-  ) private {
-    _balances[_user][_rewardToken] = _balances[_user][_rewardToken] + _amountReward;
-    firstBuy[_user][_tokenA] = true;
-  }
-
   function getSwapList(address _referrer) external view returns (SwapInfo[] memory result) {
     result = swapList[_referrer];
   }
 
   function getRewardTokens() external view returns (address[] memory result) {
     result = rewardTokens;
+  }
+
+  function setFirstBuyFee(address _token, uint256 _fee) external onlyOwner {
+    require(_fee <= feeDenominator, "Wrong Fee");
+    firstBuyFee[_token] = _fee;
+  }
+
+  function setDevAddress(address _devAddr) external onlyOwner {
+    devAddr = _devAddr;
+  }
+
+  function setRouter(address _router) external onlyOwner {
+    router = _router;
+  }
+
+  function setFeeInfo(
+    address _pair,
+    address _rewardToken,
+    uint256 _refFee,
+    uint256 _devFee
+  ) external onlyOwner {
+    require(_refFee + _devFee <= feeDenominator, "Wrong Fee");
+    pairInfo[_pair].tokenR = _rewardToken;
+    pairInfo[_pair].refFee = _refFee;
+    pairInfo[_pair].devFee = _devFee;
   }
 }

571ce71aef34ad79adeaeba561c6999c2162ed0c
erwin-huang <erwinhuank@gmail.com>
2022-01-23 07:21:44 +0700

    fix typo: factory address to router address

--- src/contracts/SummitswapMiddleman.sol
+++ src/contracts/SummitswapMiddleman.sol
@@ -27,7 +27,7 @@ contract SummitswapMiddleman is Ownable {
     summitReferral = _summitReferral;
   }
 
-  // **** Pancake SWAP ****
+  // **** SWAP ****
   // requires the initial amount to have already been sent to the first pair
   function _swap(uint256[] memory amounts, address[] memory path) internal virtual {
     for (uint256 i; i < path.length - 1; i++) {
@@ -40,83 +40,83 @@ contract SummitswapMiddleman is Ownable {
   }
 
   function swapExactTokensForTokens(
-    address factory,
+    address router,
     uint256 amountIn,
     uint256 amountOutMin,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external returns (uint256[] memory amounts) {
-    amounts = IRouter(factory).swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
+    amounts = IRouter(router).swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
     _swap(amounts, path);
   }
 
   function swapTokensForExactTokens(
-    address factory,
+    address router,
     uint256 amountOut,
     uint256 amountInMax,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(factory).swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
+    amounts = IRouter(router).swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
     _swap(amounts, path);
   }
 
   function swapExactETHForTokens(
-    address factory,
+    address router,
     uint256 amountOutMin,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external payable virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(factory).swapExactETHForTokens{value: msg.value}(amountOutMin, path, to, deadline);
+    amounts = IRouter(router).swapExactETHForTokens{value: msg.value}(amountOutMin, path, to, deadline);
     _swap(amounts, path);
   }
 
   function swapTokensForExactETH(
-    address factory,
+    address router,
     uint256 amountOut,
     uint256 amountInMax,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(factory).swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
+    amounts = IRouter(router).swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
     _swap(amounts, path);
   }
 
   function swapExactTokensForETH(
-    address factory,
+    address router,
     uint256 amountIn,
     uint256 amountOutMin,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(factory).swapExactTokensForETH(amountIn, amountOutMin, path, to, deadline);
+    amounts = IRouter(router).swapExactTokensForETH(amountIn, amountOutMin, path, to, deadline);
     _swap(amounts, path);
   }
 
   function swapETHForExactTokens(
-    address factory,
+    address router,
     uint256 amountOut,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external payable virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(factory).swapETHForExactTokens{value: msg.value}(amountOut, path, to, deadline);
+    amounts = IRouter(router).swapETHForExactTokens{value: msg.value}(amountOut, path, to, deadline);
     _swap(amounts, path);
   }
 
   // **** SWAP (supporting fee-on-transfer tokens) ****
   // requires the initial amount to have already been sent to the first pair
-  function _swapSupportingFeeOnTransferTokens(address factory, address[] memory path) internal virtual {
+  function _swapSupportingFeeOnTransferTokens(address router, address[] memory path) internal virtual {
     for (uint256 i; i < path.length - 1; i++) {
       (address input, address output) = (path[i], path[i + 1]);
       (address token0, ) = SummitswapLibrary.sortTokens(input, output);
-      address pancakeFactory = IRouter(factory).factory();
-      address pairAddress = ISummitswapFactory(pancakeFactory).getPair(input, output);
+      address factory = IRouter(router).factory();
+      address pairAddress = ISummitswapFactory(factory).getPair(input, output);
       ISummitswapPair pair = ISummitswapPair(pairAddress);
       uint256 amountInput;
       uint256 amountOutput;
@@ -134,83 +134,42 @@ contract SummitswapMiddleman is Ownable {
   }
 
   function swapExactTokensForTokensSupportingFeeOnTransferTokens(
-    address factory,
+    address router,
     uint256 amountIn,
     uint256 amountOutMin,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external virtual {
-    IRouter(factory).swapExactTokensForTokensSupportingFeeOnTransferTokens(amountIn, amountOutMin, path, to, deadline);
-    _swapSupportingFeeOnTransferTokens(factory, path);
+    IRouter(router).swapExactTokensForTokensSupportingFeeOnTransferTokens(amountIn, amountOutMin, path, to, deadline);
+    _swapSupportingFeeOnTransferTokens(router, path);
   }
 
   function swapExactETHForTokensSupportingFeeOnTransferTokens(
-    address factory,
+    address router,
     uint256 amountOutMin,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external payable virtual {
-    IRouter(factory).swapExactETHForTokensSupportingFeeOnTransferTokens{value: msg.value}(
+    IRouter(router).swapExactETHForTokensSupportingFeeOnTransferTokens{value: msg.value}(
       amountOutMin,
       path,
       to,
       deadline
     );
-    _swapSupportingFeeOnTransferTokens(factory, path);
+    _swapSupportingFeeOnTransferTokens(router, path);
   }
 
   function swapExactTokensForETHSupportingFeeOnTransferTokens(
-    address factory,
+    address router,
     uint256 amountIn,
     uint256 amountOutMin,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external virtual {
-    IRouter(factory).swapExactTokensForETHSupportingFeeOnTransferTokens(amountIn, amountOutMin, path, to, deadline);
-    _swapSupportingFeeOnTransferTokens(factory, path);
-  }
-
-  // **** LIBRARY FUNCTIONS ****
-  function quote(
-    uint256 amountA,
-    uint256 reserveA,
-    uint256 reserveB
-  ) public pure virtual returns (uint256 amountB) {
-    return SummitswapLibrary.quote(amountA, reserveA, reserveB);
-  }
-
-  function getAmountOut(
-    uint256 amountIn,
-    uint256 reserveIn,
-    uint256 reserveOut
-  ) public pure virtual returns (uint256 amountOut) {
-    return SummitswapLibrary.getAmountOut(amountIn, reserveIn, reserveOut);
-  }
-
-  function getAmountIn(
-    uint256 amountOut,
-    uint256 reserveIn,
-    uint256 reserveOut
-  ) public pure virtual returns (uint256 amountIn) {
-    return SummitswapLibrary.getAmountIn(amountOut, reserveIn, reserveOut);
-  }
-
-  function getAmountsOut(
-    address factory,
-    uint256 amountIn,
-    address[] memory path
-  ) public view virtual returns (uint256[] memory amounts) {
-    return SummitswapLibrary.getAmountsOut(factory, amountIn, path);
-  }
-
-  function getAmountsIn(
-    address factory,
-    uint256 amountOut,
-    address[] memory path
-  ) public view virtual returns (uint256[] memory amounts) {
-    return SummitswapLibrary.getAmountsIn(factory, amountOut, path);
+    IRouter(router).swapExactTokensForETHSupportingFeeOnTransferTokens(amountIn, amountOutMin, path, to, deadline);
+    _swapSupportingFeeOnTransferTokens(router, path);
   }
 }

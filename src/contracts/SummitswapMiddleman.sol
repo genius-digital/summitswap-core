056affa45e4e6cc9036b3f61766ff8a0162190d3
erwin-huang <erwinhuank@gmail.com>
2022-01-28 12:02:48 +0700

    update SummitswapMiddleman: add weth and factory as parameter

--- src/contracts/SummitswapMiddleman.sol
+++ src/contracts/SummitswapMiddleman.sol
@@ -1,6 +1,7 @@
 pragma solidity =0.6.6;
 
 import "./libraries/SummitswapLibrary.sol";
+import "./libraries/TransferHelper.sol";
 
 import "./interfaces/IRouter.sol";
 import "./interfaces/ISummitswapRouter02.sol";
@@ -8,6 +9,7 @@ import "./interfaces/ISummitReferral.sol";
 import "./interfaces/IERC20.sol";
 import "./interfaces/ISummitswapFactory.sol";
 import "./interfaces/ISummitswapPair.sol";
+import "./interfaces/IWETH.sol";
 
 import "./libraries/SafeMath2.sol";
 
@@ -19,7 +21,7 @@ contract SummitswapMiddleman is Ownable {
   address public summitReferral;
 
   modifier ensure(uint256 deadline) {
-    require(deadline >= block.timestamp, "SummitswapMiddleman: EXPIRED");
+    require(deadline >= block.timestamp, "SummitswapRouter02: EXPIRED");
     _;
   }
 
@@ -27,149 +29,37 @@ contract SummitswapMiddleman is Ownable {
     summitReferral = _summitReferral;
   }
 
-  // **** SWAP ****
-  // requires the initial amount to have already been sent to the first pair
-  function _swap(uint256[] memory amounts, address[] memory path) internal virtual {
-    for (uint256 i; i < path.length - 1; i++) {
-      (address input, address output) = (path[i], path[i + 1]);
-      uint256 amountOut = amounts[i + 1];
-      if (summitReferral != address(0)) {
-        ISummitReferral(summitReferral).swap(msg.sender, input, output, amounts[i], amountOut);
-      }
-    }
-  }
-
-  function swapExactTokensForTokens(
-    address router,
-    uint256 amountIn,
-    uint256 amountOutMin,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external returns (uint256[] memory amounts) {
-    amounts = IRouter(router).swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
-    _swap(amounts, path);
-  }
-
-  function swapTokensForExactTokens(
-    address router,
-    uint256 amountOut,
-    uint256 amountInMax,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(router).swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
-    _swap(amounts, path);
-  }
-
-  function swapExactETHForTokens(
-    address router,
-    uint256 amountOutMin,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external payable virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(router).swapExactETHForTokens{value: msg.value}(amountOutMin, path, to, deadline);
-    _swap(amounts, path);
-  }
-
-  function swapTokensForExactETH(
-    address router,
-    uint256 amountOut,
-    uint256 amountInMax,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(router).swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
-    _swap(amounts, path);
-  }
-
-  function swapExactTokensForETH(
-    address router,
-    uint256 amountIn,
-    uint256 amountOutMin,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(router).swapExactTokensForETH(amountIn, amountOutMin, path, to, deadline);
-    _swap(amounts, path);
-  }
-
-  function swapETHForExactTokens(
-    address router,
-    uint256 amountOut,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external payable virtual returns (uint256[] memory amounts) {
-    amounts = IRouter(router).swapETHForExactTokens{value: msg.value}(amountOut, path, to, deadline);
-    _swap(amounts, path);
-  }
-
-  // **** SWAP (supporting fee-on-transfer tokens) ****
-  // requires the initial amount to have already been sent to the first pair
-  function _swapSupportingFeeOnTransferTokens(address router, address[] memory path) internal virtual {
+  function _swap(
+    address factory,
+    uint256[] memory amounts,
+    address[] memory path,
+    address _to
+  ) internal virtual {
     for (uint256 i; i < path.length - 1; i++) {
       (address input, address output) = (path[i], path[i + 1]);
       (address token0, ) = SummitswapLibrary.sortTokens(input, output);
-      address factory = IRouter(router).factory();
-      address pairAddress = ISummitswapFactory(factory).getPair(input, output);
-      ISummitswapPair pair = ISummitswapPair(pairAddress);
-      uint256 amountInput;
-      uint256 amountOutput;
-      {
-        // scope to avoid stack too deep errors
-        (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
-        (uint256 reserveInput, uint256 reserveOutput) = input == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
-        amountInput = IERC20(input).balanceOf(address(pair)).sub(reserveInput);
-        amountOutput = IRouter(factory).getAmountOut(amountInput, reserveInput, reserveOutput);
-      }
-      if (summitReferral != address(0)) {
-        ISummitReferral(summitReferral).swap(msg.sender, input, output, amountInput, amountOutput);
-      }
+      uint256 amountOut = amounts[i + 1];
+      (uint256 amount0Out, uint256 amount1Out) = input == token0 ? (uint256(0), amountOut) : (amountOut, uint256(0));
+      address to = i < path.length - 2 ? SummitswapLibrary.pairFor(factory, output, path[i + 2]) : _to;
+      ISummitswapPair(SummitswapLibrary.pairFor(factory, input, output)).swap(amount0Out, amount1Out, to, new bytes(0));
     }
   }
 
-  function swapExactTokensForTokensSupportingFeeOnTransferTokens(
-    address router,
-    uint256 amountIn,
-    uint256 amountOutMin,
+  function swapETHForExactTokens(
+    address factory,
+    address WETH,
+    uint256 amountOut,
     address[] calldata path,
     address to,
     uint256 deadline
-  ) external virtual {
-    IRouter(router).swapExactTokensForTokensSupportingFeeOnTransferTokens(amountIn, amountOutMin, path, to, deadline);
-    _swapSupportingFeeOnTransferTokens(router, path);
-  }
-
-  function swapExactETHForTokensSupportingFeeOnTransferTokens(
-    address router,
-    uint256 amountOutMin,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external payable virtual {
-    IRouter(router).swapExactETHForTokensSupportingFeeOnTransferTokens{value: msg.value}(
-      amountOutMin,
-      path,
-      to,
-      deadline
-    );
-    _swapSupportingFeeOnTransferTokens(router, path);
-  }
-
-  function swapExactTokensForETHSupportingFeeOnTransferTokens(
-    address router,
-    uint256 amountIn,
-    uint256 amountOutMin,
-    address[] calldata path,
-    address to,
-    uint256 deadline
-  ) external virtual {
-    IRouter(router).swapExactTokensForETHSupportingFeeOnTransferTokens(amountIn, amountOutMin, path, to, deadline);
-    _swapSupportingFeeOnTransferTokens(router, path);
+  ) external payable virtual ensure(deadline) returns (uint256[] memory amounts) {
+    require(path[0] == WETH, "SummitswapRouter02: INVALID_PATH");
+    amounts = SummitswapLibrary.getAmountsIn(factory, amountOut, path);
+    require(amounts[0] <= msg.value, "SummitswapRouter02: EXCESSIVE_INPUT_AMOUNT");
+    IWETH(WETH).deposit{value: amounts[0]}();
+    assert(IWETH(WETH).transfer(SummitswapLibrary.pairFor(factory, path[0], path[1]), amounts[0]));
+    _swap(factory, amounts, path, to);
+    // refund dust eth, if any
+    if (msg.value > amounts[0]) TransferHelper.safeTransferBNB(msg.sender, msg.value - amounts[0]);
   }
 }

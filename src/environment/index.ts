b14899bb7ca3ef09a313d7ebc6a9b323354670f8
erwin-huang <erwinhuank@gmail.com>
2022-01-30 10:03:27 +0700

    environment/index.ts: add SummitswapMiddleman

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -17,6 +17,10 @@ const SummitswapReferrals = {
   "97": "0x7460E14B141e1978439C350E96862De684c91353",
 } as Record<string, string | undefined>;
 
+const SummitswapMiddlemans = {
+  "97": "0x88343503c9B9db8ce00d9dB364D7c89C295dA60E",
+} as Record<string, string | undefined>;
+
 const RpcUrls = {
   "56": "https://bsc-dataseed.binance.org/",
   "97": "https://data-seed-prebsc-1-s1.binance.org:8545/",
@@ -33,6 +37,8 @@ export const environment = {
   SUMMITSWAP_ROUTER: SummitswapRouters[hre.network.name],
   SUMMITSWAP_REFERRALS: SummitswapReferrals,
   SUMMITSWAP_REFERRAL: SummitswapReferrals[hre.network.name],
+  SUMMITSWAP_MIDDLEMANS: SummitswapMiddlemans,
+  SUMMITSWAP_MIDDLEMAN: SummitswapMiddlemans[hre.network.name],
   RPC_URLS: RpcUrls,
   RPC_URL: RpcUrls[hre.network.name],
   IS_VERIFY_SUPPORTED: VERIFY_SUPPORTED_ON.includes(hre.network.name),

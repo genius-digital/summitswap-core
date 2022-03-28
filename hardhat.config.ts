9fc8aa9cd8a3000a9fafcd72dd309479bbaa26c3
lukesamkharadze <lukesamkharadze@gmail.com>
2022-03-28 13:27:36 +0400

    add summitswap locker

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -58,6 +58,15 @@ export default {
           },
         },
       },
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     ],
   },
   networks: {

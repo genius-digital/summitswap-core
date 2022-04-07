eeb9fe754fdb5c1f9e4b3e081b016f4a5c8502be
erwin-huang <erwinhuank@gmail.com>
2022-04-07 16:04:13 +0700

    feat: add test for setAllowance

--- src/tests/RoyaltyFeeManager.test.ts
+++ src/tests/RoyaltyFeeManager.test.ts
@@ -9,7 +9,7 @@ import { ZERO_ADDRESS } from "src/environment";
 const { deployContract, provider } = waffle;
 
 describe("royaltyFeeManager", () => {
-  const [owner, otherWallet] = provider.getWallets();
+  const [owner, otherWallet, otherWallet1, otherWallet2] = provider.getWallets();
   let kapexToken: DummyToken;
   let royaltyFeeManager: RoyaltyFeeManager;
 
@@ -43,12 +43,66 @@ describe("royaltyFeeManager", () => {
     });
   });
 
-  // describe("setAllowance", async () => {
-  //   it("should revert when called by nonOwner")
-  //   it("should not use zero address ", async () => {
-  //     await expect(royaltyFeeManager.connect(otherWallet).transferOwnership(otherWallet.address)).to.be.revertedWith(
-  //       "Royalty_Fee_Manager: Whale address is the zero address"
-  //     );
-  //   });
-  // });
+  describe("setAllowance", async () => {
+    it("should revert when called by nonOwner", async () => {
+      await expect(royaltyFeeManager.connect(otherWallet).setAllowance(otherWallet.address, "0")).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+    });
+    it("should revert when setAllowance for zero address ", async () => {
+      await expect(royaltyFeeManager.setAllowance(ZERO_ADDRESS, "0")).to.be.revertedWith(
+        "Royalty_Fee_Manager: Whale address is the zero address"
+      );
+    });
+    it("should revert when setAllowance with zero sharesAmount ", async () => {
+      await expect(royaltyFeeManager.setAllowance(otherWallet.address, "0")).to.be.revertedWith(
+        "Royalty_Fee_Manager: shares amount is zero"
+      );
+    });
+    it("should be able to setAllowance", async () => {
+      await royaltyFeeManager.setAllowance(otherWallet.address, "25");
+      let sharesHistoryLastIndex = (await royaltyFeeManager.historyCount()).sub(1);
+      assert.equal((await royaltyFeeManager.missedRewards(sharesHistoryLastIndex)).toString(), "0");
+      assert.equal(await royaltyFeeManager.sharesAddressHistories(sharesHistoryLastIndex), otherWallet.address);
+      assert.equal((await royaltyFeeManager.sharesAmountHistories(sharesHistoryLastIndex)).toString(), "25");
+      assert.equal((await royaltyFeeManager.walletShares(otherWallet.address)).toString(), "25");
+      assert.equal((await royaltyFeeManager.totalShares()).toString(), "25");
+
+      await kapexToken.transfer(royaltyFeeManager.address, "50");
+
+      await royaltyFeeManager.setAllowance(otherWallet1.address, "10");
+      sharesHistoryLastIndex = (await royaltyFeeManager.historyCount()).sub(1);
+      assert.equal((await royaltyFeeManager.missedRewards(sharesHistoryLastIndex)).toString(), "50");
+      assert.equal(await royaltyFeeManager.sharesAddressHistories(sharesHistoryLastIndex), otherWallet1.address);
+      assert.equal((await royaltyFeeManager.sharesAmountHistories(sharesHistoryLastIndex)).toString(), "10");
+      assert.equal((await royaltyFeeManager.walletShares(otherWallet1.address)).toString(), "10");
+      assert.equal((await royaltyFeeManager.totalShares()).toString(), "35");
+
+      await kapexToken.transfer(royaltyFeeManager.address, "50");
+
+      await royaltyFeeManager.setAllowance(otherWallet2.address, "15");
+      sharesHistoryLastIndex = (await royaltyFeeManager.historyCount()).sub(1);
+      assert.equal((await royaltyFeeManager.missedRewards(sharesHistoryLastIndex)).toString(), "100");
+      assert.equal(await royaltyFeeManager.sharesAddressHistories(sharesHistoryLastIndex), otherWallet2.address);
+      assert.equal((await royaltyFeeManager.sharesAmountHistories(sharesHistoryLastIndex)).toString(), "15");
+      assert.equal((await royaltyFeeManager.walletShares(otherWallet2.address)).toString(), "15");
+      assert.equal((await royaltyFeeManager.totalShares()).toString(), "50");
+
+      await kapexToken.transfer(royaltyFeeManager.address, "50");
+
+      await royaltyFeeManager.setAllowance(otherWallet.address, "35");
+      sharesHistoryLastIndex = (await royaltyFeeManager.historyCount()).sub(1);
+      assert.equal((await royaltyFeeManager.missedRewards(sharesHistoryLastIndex)).toString(), "150");
+      assert.equal(await royaltyFeeManager.sharesAddressHistories(sharesHistoryLastIndex), otherWallet.address);
+      assert.equal((await royaltyFeeManager.sharesAmountHistories(sharesHistoryLastIndex)).toString(), "10");
+      assert.equal((await royaltyFeeManager.walletShares(otherWallet.address)).toString(), "35");
+
+      await royaltyFeeManager.setAllowance(otherWallet.address, "25");
+      sharesHistoryLastIndex = (await royaltyFeeManager.historyCount()).sub(1);
+      assert.equal((await royaltyFeeManager.missedRewards(sharesHistoryLastIndex)).toString(), "150");
+      assert.equal(await royaltyFeeManager.sharesAddressHistories(sharesHistoryLastIndex), otherWallet.address);
+      assert.equal((await royaltyFeeManager.sharesAmountHistories(sharesHistoryLastIndex)).toString(), "0");
+      assert.equal((await royaltyFeeManager.walletShares(otherWallet.address)).toString(), "25");
+    });
+  });
 });

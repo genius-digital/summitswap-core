21b862de64b7d9e5f67ac73927e5215d3012ec1c
sxio <stephen.huang96@gmail.com>
2022-07-01 11:00:25 +0700

    refactor deploySummitPresaleFactory

--- src/scripts/deploySummitPresaleFactory.ts
+++ src/scripts/deploySummitPresaleFactory.ts
@@ -1,25 +1,27 @@
+import { BigNumber } from "ethers";
+import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { tryVerify } from "./utils/verify";
 
-export async function deploySummitPresaleFactory(feeToSetter: string, serviceFeeAddress: string) {
+export async function deploySummitPresaleFactory(serviceFee: BigNumber, serviceFeeAddress: string) {
   console.log("Starting to deploy SummitPresaleFactory");
 
   const SummitFactoryPresale = await ethers.getContractFactory("SummitFactoryPresale");
-  const summitFactoryPresale = await SummitFactoryPresale.deploy(feeToSetter, serviceFeeAddress);
+  const summitFactoryPresale = await SummitFactoryPresale.deploy(serviceFee, serviceFeeAddress);
   await summitFactoryPresale.deployed();
 
   console.log("summitFactoryPresale deployed to:", summitFactoryPresale.address);
 
-  await tryVerify(summitFactoryPresale.address, [feeToSetter, serviceFeeAddress]);
+  await tryVerify(summitFactoryPresale.address, [serviceFee, serviceFeeAddress]);
 
   return summitFactoryPresale;
 }
 
 async function main() {
-  const createPresaleFee = "100000000000000"; // 0.0001 ether
+  const serviceFee = parseEther("0.0001");
   const serviceFeeReciever = "0xE01C1Cd3c0a544adF8cB764dCCF855bcE4943B1F";
 
-  await deploySummitPresaleFactory(createPresaleFee, serviceFeeReciever);
+  await deploySummitPresaleFactory(serviceFee, serviceFeeReciever);
 }
 
 if (require.main === module) {

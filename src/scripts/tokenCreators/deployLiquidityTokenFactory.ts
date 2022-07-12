4b608136bb15d9a3192d613073b55a35e7560e03
umaismateen <umais.mateen@gmail.com>
2022-07-12 14:41:45 +0500

    rename token factories

--- src/scripts/tokenCreators/deployLiquidityTokenFactory.ts
+++ src/scripts/tokenCreators/deployLiquidityTokenFactory.ts
@@ -3,14 +3,14 @@ import { BigNumber } from "ethers";
 import { parseEther } from "ethers/lib/utils";
 import { tryVerify } from "../utils/verify";
 
-export async function deployTokenFactory(serviceFee: BigNumber, serviceFeeAddress: string) {
+export async function deployLiquidityTokenFactory(serviceFee: BigNumber, serviceFeeAddress: string) {
   console.log("Starting to deploy LiquidityTokenFactory");
 
   const TokenFactoryContract = await ethers.getContractFactory("LiquidityTokenFactory");
   const tokenFactoryDeploy = await TokenFactoryContract.deploy(serviceFee, serviceFeeAddress);
   await tokenFactoryDeploy.deployed();
 
-  console.log("LiquidityFactory deployed to:", tokenFactoryDeploy.address);
+  console.log("LiquidityTokenFactory deployed to:", tokenFactoryDeploy.address);
 
   await tryVerify(tokenFactoryDeploy.address, [serviceFee, serviceFeeAddress]);
 
@@ -19,8 +19,8 @@ export async function deployTokenFactory(serviceFee: BigNumber, serviceFeeAddres
 
 async function main() {
   const serviceFee = parseEther("0.0001");
-  const serviceFeeReciever = "0xE01C1Cd3c0a544adF8cB764dCCF855bcE4943B1F";
-  await deployTokenFactory(serviceFee, serviceFeeReciever);
+  const serviceFeeReciever = "0x5f8397444c02c02BD1F20dAbAB42AFCdf396dacA";
+  await deployLiquidityTokenFactory(serviceFee, serviceFeeReciever);
 }
 
 if (require.main === module) {
--- src/scripts/tokenCreators/deployStandardTokenFactory.ts
+++ src/scripts/tokenCreators/deployStandardTokenFactory.ts
@@ -3,14 +3,14 @@ import { ethers } from "hardhat";
 import { parseEther } from "ethers/lib/utils";
 import { tryVerify } from "../utils/verify";
 
-export async function deployTokenFactory(serviceFee: BigNumber, serviceFeeAddress: string) {
+export async function deployStandardTokenFactory(serviceFee: BigNumber, serviceFeeAddress: string) {
   console.log("Starting to deploy StandardTokenFactory");
 
   const TokenFactoryContract = await ethers.getContractFactory("StandardTokenFactory");
   const tokenFactoryDeploy = await TokenFactoryContract.deploy(serviceFee, serviceFeeAddress);
   await tokenFactoryDeploy.deployed();
 
-  console.log("StandardFactory deployed to:", tokenFactoryDeploy.address);
+  console.log("StandardTokenFactory deployed to:", tokenFactoryDeploy.address);
 
   await tryVerify(tokenFactoryDeploy.address, [serviceFee, serviceFeeAddress]);
 
@@ -19,8 +19,8 @@ export async function deployTokenFactory(serviceFee: BigNumber, serviceFeeAddres
 
 async function main() {
   const serviceFee = parseEther("0.0001");
-  const serviceFeeReciever = "0xE01C1Cd3c0a544adF8cB764dCCF855bcE4943B1F";
-  await deployTokenFactory(serviceFee, serviceFeeReciever);
+  const serviceFeeReciever = "0x5f8397444c02c02BD1F20dAbAB42AFCdf396dacA";
+  await deployStandardTokenFactory(serviceFee, serviceFeeReciever);
 }
 
 if (require.main === module) {

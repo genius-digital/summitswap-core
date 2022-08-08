dcc1ff71cd4100116aeb26eac43151bb4081a708
sxio <stephen.huang96@gmail.com>
2022-08-08 09:58:56 +0700

    feat(SummitWhitelabelNft.test): fix tokenURI cases

--- src/tests/SummitWhitelabelNft.test.ts
+++ src/tests/SummitWhitelabelNft.test.ts
@@ -237,19 +237,20 @@ describe("SummitWhitelabelNft", () => {
 
   describe("tokenURI", () => {
     it("should revert if tokenId not exists", async () => {
-      await expect(await summitWhitelabelNft.tokenURI(0)).to.be.revertedWith("tokenId not exists");
+      await expect(summitWhitelabelNft.tokenURI(0)).to.be.revertedWith("tokenId not exists");
     });
     it("should return tokenURI", async () => {
-      await summitWhitelabelNft.enterPublicPhase();
+      await summitWhitelabelNft.connect(nftOwner).enterPublicPhase();
 
       const mintPrice = (await summitWhitelabelNft.tokenInfo()).publicMintPrice;
-      await summitWhitelabelNft["mint(uint256)"](mintAmount, {
+      await summitWhitelabelNft.connect(minter)["mint(uint256)"](mintAmount, {
         value: mintPrice.mul(mintAmount),
       });
 
       const tokenId = (await summitWhitelabelNft.tokensOfOwner(minter.address))[0];
+      const tokenUri = `${baseUri + tokenId.toString()}.json`;
 
-      assert.equal(await summitWhitelabelNft.tokenURI(tokenId), baseUri + mintAmount.toString());
+      assert.equal(await summitWhitelabelNft.tokenURI(tokenId), tokenUri);
     });
   });
 });

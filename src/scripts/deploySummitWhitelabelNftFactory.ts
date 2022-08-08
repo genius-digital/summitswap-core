12bac7e3efa6a95c0f63598cf94865d3411d925b
sxio <stephen.huang96@gmail.com>
2022-08-08 11:55:07 +0700

    feat(SummitWhitelabelNft): deploy script

---
+++ src/scripts/deploySummitWhitelabelNftFactory.ts
@@ -0,0 +1,29 @@
+import { BigNumber } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { tryVerify } from "./utils/verify";
+
+export async function deployWhitelabelNftFactory(serviceFee: BigNumber, serviceFeeReceiver: string) {
+  console.log("Starting to deploy SummitWhitelabelNftFactory");
+
+  const SummitWhitelabelNftFactory = await ethers.getContractFactory("SummitWhitelabelNftFactory");
+  const summitWhitelabelNftFactory = await SummitWhitelabelNftFactory.deploy(serviceFee, serviceFeeReceiver);
+  await summitWhitelabelNftFactory.deployed();
+
+  console.log("SummitWhitelabelNftFactory deployed to:", summitWhitelabelNftFactory.address);
+
+  await tryVerify(summitWhitelabelNftFactory.address, [serviceFee, serviceFeeReceiver]);
+
+  return summitWhitelabelNftFactory;
+}
+
+async function main() {
+  const serviceFee = parseEther("0.001");
+  const serviceFeeReceiver = "0xEF0c0cCC6F249352B32290E47023E992f7E7592C";
+
+  await deployWhitelabelNftFactory(serviceFee, serviceFeeReceiver);
+}
+
+if (require.main === module) {
+  main();
+}

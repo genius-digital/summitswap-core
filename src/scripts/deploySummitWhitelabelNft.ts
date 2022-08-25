33b91ca9340e6dd620a142ddb4ac7463e4f4e7d0
sxio <stephen.huang96@gmail.com>
2022-08-25 12:15:04 +0700

    feat(SummitWhitelabelNft): adjust deploy script

--- src/scripts/deploySummitWhitelabelNft.ts
+++ src/scripts/deploySummitWhitelabelNft.ts
@@ -6,8 +6,10 @@ import { tryVerify } from "./utils/verify";
 export async function deploySummitWhitelabelNft(tokenInfo: TokenInfoStruct, initialUri: string, owner: string) {
   console.log("Starting to deploy SummitWhitelabelNft");
 
+  const signerAddress = "0x3D2d8991370e34dd34b69B4CA09981d69146dFC1";
+
   const SummitWhitelabelNft = await ethers.getContractFactory("SummitWhitelabelNft");
-  const summitWhitelabelNft = await SummitWhitelabelNft.deploy(tokenInfo, initialUri, owner);
+  const summitWhitelabelNft = await SummitWhitelabelNft.deploy(tokenInfo, initialUri, owner, signerAddress);
   await summitWhitelabelNft.deployed();
 
   console.log("SummitWhitelabelNft deployed to:", summitWhitelabelNft.address);
@@ -24,7 +26,6 @@ async function main() {
     maxSupply: 999,
     whitelistMintPrice: parseEther("0.001"),
     publicMintPrice: parseEther("0.002"),
-    signer: "0x5f8397444c02c02BD1F20dAbAB42AFCdf396dacA",
     phase: 0,
   };
   const initialUri = "ipfs://QmSAo4kt2N9mdgwTF5MREgSWHoF3CxwwmbhZV5M3u83SVg/";
--- src/scripts/deploySummitWhitelabelNftFactory.ts
+++ src/scripts/deploySummitWhitelabelNftFactory.ts
@@ -6,13 +6,19 @@ import { tryVerify } from "./utils/verify";
 export async function deploySummitWhitelabelNftFactory(serviceFee: BigNumber, serviceFeeReceiver: string) {
   console.log("Starting to deploy SummitWhitelabelNftFactory");
 
+  const signerAddress = "0x3D2d8991370e34dd34b69B4CA09981d69146dFC1";
+
   const SummitWhitelabelNftFactory = await ethers.getContractFactory("SummitWhitelabelNftFactory");
-  const summitWhitelabelNftFactory = await SummitWhitelabelNftFactory.deploy(serviceFee, serviceFeeReceiver);
+  const summitWhitelabelNftFactory = await SummitWhitelabelNftFactory.deploy(
+    serviceFee,
+    serviceFeeReceiver,
+    signerAddress
+  );
   await summitWhitelabelNftFactory.deployed();
 
   console.log("SummitWhitelabelNftFactory deployed to:", summitWhitelabelNftFactory.address);
 
-  await tryVerify(summitWhitelabelNftFactory.address, [serviceFee, serviceFeeReceiver]);
+  await tryVerify(summitWhitelabelNftFactory.address, [serviceFee, serviceFeeReceiver, signerAddress]);
 
   return summitWhitelabelNftFactory;
 }

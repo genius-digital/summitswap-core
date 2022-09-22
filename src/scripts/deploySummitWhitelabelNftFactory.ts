52cfac2058f5ef303141e8f4bef4955612f37bd0
sxio <stephen.huang96@gmail.com>
2022-09-22 15:41:03 +0700

    feat(whitelabel nft): adjust deploy srcipt

--- src/scripts/deploySummitWhitelabelNftFactory.ts
+++ src/scripts/deploySummitWhitelabelNftFactory.ts
@@ -3,26 +3,27 @@ import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { tryVerify } from "./utils/verify";
 
-export async function deploySummitWhitelabelNftFactory(serviceFee: BigNumber) {
+export async function deploySummitWhitelabelNftFactory(serviceFee: BigNumber, withdrawFee: BigNumber) {
   console.log("Starting to deploy SummitWhitelabelNftFactory");
 
   const signerAddress = "0x3D2d8991370e34dd34b69B4CA09981d69146dFC1";
 
   const SummitWhitelabelNftFactory = await ethers.getContractFactory("SummitWhitelabelNftFactory");
-  const summitWhitelabelNftFactory = await SummitWhitelabelNftFactory.deploy(serviceFee, signerAddress);
+  const summitWhitelabelNftFactory = await SummitWhitelabelNftFactory.deploy(serviceFee, withdrawFee, signerAddress);
   await summitWhitelabelNftFactory.deployed();
 
   console.log("SummitWhitelabelNftFactory deployed to:", summitWhitelabelNftFactory.address);
 
-  await tryVerify(summitWhitelabelNftFactory.address, [serviceFee, signerAddress]);
+  await tryVerify(summitWhitelabelNftFactory.address, [serviceFee, withdrawFee, signerAddress]);
 
   return summitWhitelabelNftFactory;
 }
 
 async function main() {
   const serviceFee = parseEther("0.001");
+  const withdrawFee = parseEther("0.001");
 
-  await deploySummitWhitelabelNftFactory(serviceFee);
+  await deploySummitWhitelabelNftFactory(serviceFee, withdrawFee);
 }
 
 if (require.main === module) {
